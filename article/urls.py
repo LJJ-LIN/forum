86bcea895ce782216f5051b214b6c0239e779a39
LJJ-LIN <672731931@qq.com>
2018-03-13 23:01:01 +0800

    创建文章页面

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from article.views import article_list
+from article.views import article_list,article_create
+
 
 urlpatterns = [
-      url(r'^list/(?P<block_id>\d+)',article_list)
+      url(r'^list/(?P<block_id>\d+)',article_list),
+      url(r'^create/(?P<block_id>\d+)',article_create),
 ]
